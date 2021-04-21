e44f8113a9495699cf88af675295ac90800bd727
JoaoMarcelino <j.13.marcelino@gmail.com>
2021-04-21 17:10:49 +0100

    problemaB

---
+++ lego.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <sstream>
+
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include <math.h>
+
+using namespace std;
+
+/*
+ * number_blocks = n
+ * size_block = h
+ * max_height = H
+ */
+int number_blocks, size_block, max_height;
+
+int tabela_h, tabela_w;
+vector<vector<int>> tabelha;
+vector<vector<int>> soma;
+
+int mod =  1000000007; 
+
+// FUNCÕES DISPONIBILIZADAS PELO PROFESSOR
+
+int mod_abs(int a, int mod) {
+ 	return ((a % mod) + mod) % mod;
+}
+
+int mod_add(int a, int b, int mod) {
+ 	return (mod_abs(a, mod) + mod_abs(b, mod)) % mod;
+}
+
+int mod_sub(int a, int b, int mod) {
+	return mod_add(a, -b, mod);
+}
+
+void printTabelha(vector<vector<int>> tabelha ,int width, int height)
+{
+    for (int i = 0; i < width; i++)
+	{
+        for (int j = 0; j < height; j++)
+            cout << tabelha[i][j] << " ";
+			
+        cout << endl;
+    }
+}
+
+int TabelhaAdd(int fila, int coluna)
+{
+    int soma = 0;
+    
+	for (int i = 0; i < size_block - 1; i++) 
+	{
+		if (fila - i == 0)
+			break;
+
+		soma += tabelha[fila - 1 - i][coluna - 1];
+	}
+
+    return tabelha[fila][coluna] = soma;
+}
+
+int SomaAdd(int fila, int coluna)
+{
+    return soma[fila][coluna] = soma[fila][coluna - 1] + tabelha[fila][coluna];
+}
+
+void addArcs(int &count, int linha, int coluna){
+    /*int aux += TabelhaAdd(linha, coluna);
+
+	soma[linha][coluna] = aux;
+    */
+    int aux = TabelhaAdd(linha, coluna);
+     SomaAdd(linha, coluna);
+    for (int i = 1; i < size_block ; i++){
+
+        if (linha - i < 0)
+			break;
+
+        /*
+        cout << "tabela "<< linha +  size_block << " : "<< coluna+1 <<" (" << aux << ")";
+        cout <<"multiplicado por soma " << linha - i   +  size_block<< " : "<< number_blocks - coluna - 1 <<" (" << soma[linha - i][number_blocks - coluna -2] << ")\n";
+        */
+        count += aux * soma[linha - i][number_blocks - coluna -2];
+    }
+
+}
+
+
+int ArchBuilder(){
+
+    tabelha[0][0] = 1;
+    soma[0] = vector(tabela_w, 1); 
+    int count = 0;
+    for (int i = 1; i < tabela_w; i++){
+        for (int j = 1; j < tabela_h; j++){
+            
+            addArcs(count, i , j);
+            //cout << "Contas de " << i <<" + " << j<<": " << count << endl;
+            
+        }
+    }
+    /*
+    cout << "Tabelha" << endl;
+    printTabelha(tabelha,max_height - size_block + 1, number_blocks);
+    cout<< "Soma" << endl;
+    printTabelha(soma, max_height - size_block + 1, number_blocks);
+    */
+    return count;
+}
+
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int number_cases, result;
+    cin >> number_cases;
+
+    for(int i = 0; i < number_cases; i++){
+
+        cin >> number_blocks >> size_block >> max_height;
+
+        tabela_w = max_height - size_block + 1;
+        tabela_h = number_blocks;
+
+        tabelha = vector<vector<int>>(tabela_w, vector<int>(tabela_h, 0));
+        soma = vector<vector<int>>(tabela_w, vector<int>(tabela_h, 0));
+
+        result = ArchBuilder();
+
+        cout << result << endl;
+    }
+
+    return 0;
+}
