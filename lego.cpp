9c7c7acab3e0985ebb909567fd58adf82b707052
JoaoMarcelino <j.13.marcelino@gmail.com>
2021-04-22 18:12:06 +0100

    140

--- lego.cpp
+++ lego.cpp
@@ -20,106 +20,125 @@ using namespace std;
 int number_blocks, size_block, max_height;
 
 int tabela_h, tabela_w;
-vector<vector<int>> tabelha;
-vector<vector<int>> soma;
+vector<vector<unsigned int>> tabelha;
+vector<vector<unsigned int>> soma;
 
 //1 000 000 007
-int mod =  1000000007; 
+unsigned long int  mod =  1000000007; 
 
 // FUNCÕES DISPONIBILIZADAS PELO PROFESSOR
 
-int mod_abs(int a, int mod) {
+unsigned long int  mod_abs(unsigned long int  a) {
  	return ((a % mod) + mod) % mod;
 }
 
-int mod_add(int a, int b, int mod) {
- 	return (mod_abs(a, mod) + mod_abs(b, mod)) % mod;
+unsigned long int  mod_add(unsigned long int  a, unsigned long int  b) {
+ 	return (mod_abs(a) + mod_abs(b)) % mod;
 }
 
-int mod_sub(int a, int b, int mod) {
-	return mod_add(a, -b, mod);
+unsigned long int  mod_sub(unsigned long int  a, unsigned long int  b) {
+	return mod_add(a, -b);
 }
 
-void printTabelha(vector<vector<int>> tabelha ,int width, int height)
+unsigned long int  mod_mult(unsigned long int  a, unsigned long int  b) {
+ 	return mod_abs((mod_abs(a) * mod_abs(b)));
+}
+
+void printTabelha(vector<vector<unsigned int>> tabelha , int  width,  int  height)
 {
-    for (int i = 0; i < width; i++)
+    for ( int  i = 0; i < width; i++)
 	{
-        for (int j = 0; j < height; j++)
+        for ( int  j = 0; j < height; j++)
             cout << tabelha[i][j] << " ";
-			
         cout << endl;
     }
 }
 
-int TabelhaAdd(int fila, int coluna)
+unsigned long int  TabelhaAdd( int  fila, int  coluna)
 {
-    int soma = 0;
-    
-	for (int i = 0; i < size_block - 1; i++) 
+    unsigned long int  soma = 0;
+
+	for ( int  i = 0; i < size_block - 1; i++) 
 	{
 		if (fila - i == 0)
 			break;
 
-		soma += tabelha[fila - 1 - i][coluna - 1];
+		//soma += tabelha[fila - 1 - i][coluna - 1];
+        soma =  mod_add(soma, tabelha[fila - 1 - i][coluna - 1]);
 	}
 
     return tabelha[fila][coluna] = soma;
 }
 
 
-int addArcs(int &row_count, int linha, int coluna){
-    int count = 0;
-    int aux = TabelhaAdd(linha, coluna);
-    row_count= row_count + aux;
-
-	soma[linha][coluna] = row_count;
-    
+unsigned long int  addArcs(unsigned long int  &row_count, int  linha, int  coluna){
+    unsigned long int  count = 0;
+    unsigned long int  aux = TabelhaAdd(linha, coluna);
 
     if(aux == 0){
+        soma[linha][coluna] = row_count;
         return 0;
     }
 
-    for (int i = 1; i < size_block ; i++){
+    row_count = mod_add(row_count, aux); //claramente faz coisas
+    //row_count= row_count + aux;
+
+	soma[linha][coluna] = row_count;
+    
+    for (int  i = 1; i < size_block ; i++){
 
         if (linha - i < 0)
             break;
 
-        /*
-        cout << "tabela "<< linha +  size_block << " : "<< coluna+1 <<" (" << aux << ")";
-        cout <<"multiplicado por soma " << linha - i   +  size_block<< " : "<< number_blocks - coluna - 1 <<" (" << soma[linha - i][number_blocks - coluna -2] << ")\n";
-        */
+        if (number_blocks - coluna -2 >= 0){
+            //cout << "tabela "<< linha +  size_block << " : "<< coluna+1 <<" (" << aux << ")";
+            //cout <<"multiplicado por soma " << linha - i   +  size_block<< " : "<< number_blocks - coluna - 1 <<" (" << soma[linha - i][number_blocks - coluna -2] << ")\n";
 
-        //count += aux * soma[linha - i][number_blocks - coluna -2];
-        count = mod_add(count, aux * soma[linha - i][number_blocks - coluna -2], mod);
+            //count += aux * soma[linha - i][number_blocks - coluna -2];
+            count = mod_add(count, aux * soma[linha - i][number_blocks - coluna -2]);
+        }
     }
 
-
     return count;
-
 }
 
 
-int ArchBuilder(){
+unsigned long int  ArchBuilder(){
 
     tabelha[0][0] = 1;
-    soma[0] = vector(tabela_w, 1); 
-    int count = 0;
-    for (int i = 1; i < tabela_w; i++){
-        int row_count = 0;
-        for (int j = 1; j < tabela_h; j++){
+    soma[0] = vector<unsigned int>(tabela_w, 1); 
+    unsigned long int  count = 0;
+    int initial_j = 1;
+
+    for (int  i = 1; i < tabela_w; i++){
+
+        unsigned long int  row_count = 0;
+        for (int  j = initial_j; j < tabela_h; j++){
             
+            unsigned long int arcs = addArcs(row_count, i , j);
+
+
+            count = mod_add(count, arcs);
             //count += addArcs(row_count, i , j);
-            count = mod_add(count, addArcs(row_count, i , j), mod);
             //cout << "Contas de " << i <<" + " << j<<": " << count << endl;
-            
+        }
+
+        // TODO Find first
+        //cout << "value: " << i + 3 << " i " << i << " initial_j: "<< initial_j << " total: "<< i/ (size_block - 1)<< endl;
+        if (i/ (size_block - 1) >= initial_j){
+            initial_j++;
+
+            if (initial_j>= number_blocks) break;
         }
     }
-    
+
+    /*
     cout << "Tabelha" << endl;
-    printTabelha(tabelha,max_height - size_block + 1, number_blocks);
+    printTabelha(tabelha, tabela_w, tabela_h);
     cout<< "Soma" << endl;
-    printTabelha(soma, max_height - size_block + 1, number_blocks);
-    
+    printTabelha(soma, tabela_w, tabela_h);
+    */
+   
     return count;
 }
 
@@ -129,18 +148,18 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int number_cases, result;
+    int  number_cases, result;
     cin >> number_cases;
 
-    for(int i = 0; i < number_cases; i++){
+    for(int  i = 0; i < number_cases; i++){
 
         cin >> number_blocks >> size_block >> max_height;
 
         tabela_w = max_height - size_block + 1;
         tabela_h = number_blocks;
 
-        tabelha = vector<vector<int>>(tabela_w, vector<int>(tabela_h, 0));
-        soma = vector<vector<int>>(tabela_w, vector<int>(tabela_h, 0));
+        tabelha = vector<vector<unsigned int>>(tabela_w, vector<unsigned int>(tabela_h, 0));
+        soma = vector<vector<unsigned int>>(tabela_w, vector<unsigned int>(tabela_h, 0));
 
         result = ArchBuilder();
 
@@ -149,3 +168,6 @@ int main(){
 
     return 0;
 }
+
+// Checkar se resultados != 0 para n fazer add's
+// Parar se for uma linha de 0's
