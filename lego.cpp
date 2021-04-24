8033e094810ea4021e9bd2e914df1c70f9797652
JoaoMarcelino <j.13.marcelino@gmail.com>
2021-04-24 17:58:51 +0100

    140 almost 200

--- lego.cpp
+++ lego.cpp
@@ -24,24 +24,20 @@ vector<vector<unsigned int>> tabelha;
 vector<vector<unsigned int>> soma;
 
 //1 000 000 007
-unsigned long int  mod =  1000000007; 
+long long int  mod =  1000000007; 
 
 // FUNCÕES DISPONIBILIZADAS PELO PROFESSOR
 
-unsigned long int  mod_abs(unsigned long int  a) {
+long long int  mod_abs(long long int  a) {
  	return ((a % mod) + mod) % mod;
 }
 
-unsigned long int  mod_add(unsigned long int  a, unsigned long int  b) {
+long long int  mod_add(long long int  a, long long int  b) {
  	return (mod_abs(a) + mod_abs(b)) % mod;
 }
 
-unsigned long int  mod_sub(unsigned long int  a, unsigned long int  b) {
+long long int  mod_sub(long long int  a, long long int  b) {
 	return mod_add(a, -b);
-}
-
-unsigned long int  mod_mult(unsigned long int  a, unsigned long int  b) {
- 	return mod_abs((mod_abs(a) * mod_abs(b)));
 }
 
 void printTabelha(vector<vector<unsigned int>> tabelha , int  width,  int  height)
@@ -54,81 +50,114 @@ void printTabelha(vector<vector<unsigned int>> tabelha , int  width,  int  heigh
     }
 }
 
-unsigned long int  TabelhaAdd( int  fila, int  coluna)
-{
-    unsigned long int  soma = 0;
 
-	for ( int  i = 0; i < size_block - 1; i++) 
-	{
-		if (fila - i == 0)
-			break;
+long long int TabelhaAdd(int fila,int coluna){
+    long long int sum = 0;
+    long long int sub = 0;
+    
+    if (fila - size_block < 0 ){
+        sub = tabelha[fila -1][coluna];
+        //cout << "sub1- "<< sub<< endl;
+    }
+    else{
+        sub =mod_sub(tabelha[fila - 1][coluna], tabelha[fila - size_block][coluna - 1]);
+    }
+    
+    sum =  mod_add(sub, tabelha[fila - 1][coluna - 1]);
 
-		//soma += tabelha[fila - 1 - i][coluna - 1];
-        soma =  mod_add(soma, tabelha[fila - 1 - i][coluna - 1]);
-	}
+    //cout << "Contas de " << fila <<" + " << coluna <<": " << sub  << " - " << soma << endl;
 
-    return tabelha[fila][coluna] = soma;
+    return tabelha[fila][coluna] = sum;
+}
+
+long long int SomaAdd(long long int row_count,int fila,int coluna){
+    long long int sum = 0;
+    long long int sub = 0;
+
+     if (fila - size_block + 1 < 0 ){
+         sub = soma[fila - 1][coluna];
+         //cout << "Contas de [" << fila <<", " << coluna <<"]: " << soma[fila - 1][coluna]  << " - "<<  0<<" + " << row_count << endl;
+    }
+    else{
+        sub =mod_sub(soma[fila - 1][coluna], soma[fila - size_block + 1][coluna]);
+        //cout << "Contas de [" << fila <<", " << coluna <<"]: " << soma[fila - 1][coluna]  << " - "<< soma[fila - size_block][coluna]<<" + " << row_count << endl;
+    }
+
+    sum=  mod_add(sub, row_count);
+
+    //cout << sum << endl;
+
+    return soma[fila][coluna] = sum;
+
+
 }
 
 
-unsigned long int  addArcs(unsigned long int  &row_count, int  linha, int  coluna){
-    unsigned long int  count = 0;
-    unsigned long int  aux = TabelhaAdd(linha, coluna);
+long long int  addArcs(long long int  &row_count, int  linha, int  coluna){
+    long long int  count = 0;
+    long long int  aux = TabelhaAdd(linha, coluna);
 
-    if(aux == 0){
-        soma[linha][coluna] = row_count;
+    long long int temp = 0;
+
+    if(aux == 0)
         return 0;
-    }
-
-    row_count = mod_add(row_count, aux); //claramente faz coisas
-    //row_count= row_count + aux;
-
-	soma[linha][coluna] = row_count;
     
-    for (int  i = 1; i < size_block ; i++){
+    row_count = mod_add(row_count, aux);
 
+    for (int  i = 1; i < size_block ; i++){
         if (linha - i < 0)
             break;
 
         if (number_blocks - coluna -2 >= 0){
-            //cout << "tabela "<< linha +  size_block << " : "<< coluna+1 <<" (" << aux << ")";
-            //cout <<"multiplicado por soma " << linha - i   +  size_block<< " : "<< number_blocks - coluna - 1 <<" (" << soma[linha - i][number_blocks - coluna -2] << ")\n";
-
-            //count += aux * soma[linha - i][number_blocks - coluna -2];
-            count = mod_add(count, aux * soma[linha - i][number_blocks - coluna -2]);
+            temp = mod_add(temp, soma[linha - i][number_blocks - coluna -2]);
         }
     }
+
+    count = mod_add(count, aux * soma[linha - 1][number_blocks - coluna -2]);/*erro*/
+    //count = mod_add(count, aux *temp);
 
     return count;
 }
 
 
-unsigned long int  ArchBuilder(){
+long long int  ArchBuilder(){
 
     tabelha[0][0] = 1;
-    soma[0] = vector<unsigned int>(tabela_w, 1); 
-    unsigned long int  count = 0;
+    soma[0] = vector<unsigned int>(tabela_w + 1, 1); 
+
+    long long int  count = 0;
     int initial_j = 1;
 
     for (int  i = 1; i < tabela_w; i++){
 
-        unsigned long int  row_count = 0;
+        long long int  row_count = 0;
+        bool isWorth = true;
+
         for (int  j = initial_j; j < tabela_h; j++){
             
-            unsigned long int arcs = addArcs(row_count, i , j);
+            if (isWorth){
 
+                long long int arcs = addArcs(row_count, i , j);
 
-            count = mod_add(count, arcs);
-            //count += addArcs(row_count, i , j);
-            //cout << "Contas de " << i <<" + " << j<<": " << count << endl;
+                if(arcs != 0){
+                    count = mod_add(count, arcs);
+                }
+
+                else
+                {
+                    //if (j == initial_j) return count; //nao da para printar com isto
+                    isWorth = false;
+                }
+
+            }
+            SomaAdd(row_count, i, j);  /*erro*/
+            //soma[i][j]= row_count;
+
         }
-
-        // TODO Find first
-        //cout << "value: " << i + 3 << " i " << i << " initial_j: "<< initial_j << " total: "<< i/ (size_block - 1)<< endl;
-        if (i/ (size_block - 1) >= initial_j){
+        if (i / (size_block - 1) >= initial_j){
             initial_j++;
 
-            if (initial_j>= number_blocks) break;
+            if (initial_j >= number_blocks) break;
         }
     }
 
@@ -137,7 +166,7 @@ unsigned long int  ArchBuilder(){
     printTabelha(tabelha, tabela_w, tabela_h);
     cout<< "Soma" << endl;
     printTabelha(soma, tabela_w, tabela_h);
-    */
+   */ 
    
     return count;
 }
@@ -168,6 +197,3 @@ int main(){
 
     return 0;
 }
-
-// Checkar se resultados != 0 para n fazer add's
-// Parar se for uma linha de 0's
